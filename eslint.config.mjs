cd3574a8859146b0b2208227576dfb0d8ee67d08
Oyasuminasai <61616918+nfnfgo@users.noreply.github.com>
2025-05-31 17:36:04 +0800

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    { ignores: ["node_modules", "./*.mjs", "./main.js"] },
+    { files: ["src/**/*.{ts,js,tsx,jsx}"] },
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+    {
+        rules: {
+            "no-unused-vars": "off",
+            "prefer-const": "error",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+            "@typescript-eslint/no-empty-function": "warn",
+        },
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+);
